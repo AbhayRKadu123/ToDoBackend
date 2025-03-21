15d92c05cb2febef26a793a65d9397a226f5beb6
Abhay Kadu <abhay@triplewsols.com>
2025-03-22 01:17:06 +0530

    second commit

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ app.listen(8080,()=>{
 })
 app.get("/gettodo",(req,res)=>{
     console.log('app is listening on port 8080')
-    res.json({msg:'app is listenng '})
+    res.json({msg:'app is listenng on port 80080  '})
 })
 app.post('/AddToDo',(req,res)=>{
     console.log(req.body)
-    res.send('add to todo ')
+    res.send('add to todo is the console to log value  ')
+})
+app.get('/login',(req,res)=>{
+    res.send('this is login btn')
 })
